8a3c4128e4cd232df9c375122436e3db3a0c45d3
Jared Hasson <jaredezzethasson@gmail.com>
2020-02-22 14:36:39 -0700

    first api test

--- script-news.js
+++ script-news.js
@@ -1,35 +1,37 @@
 function onClick(e) {
     e.preventDefault();
     // get form values
-    let number = document.getElementById('number').value;
-    let s = document.getElementById('selector');
-    let type = s.options[s.selectedIndex].value;
-
-    // check if number is empty
-    if (number === "") {
-        number = "random";
-    }
+    let searchText = document.getElementById('search-text').value;
+    console.log(searchText);
 
     // setup URL
-    let url = "http://numberapi.com/" + number + "/" + "type" + "?json";
+    let url = " http://newsapi.org/v2/everything?q="+searchText+"&from=2020-01-22&sortBy=publishedAt&apiKey=183d7f7754164e96962b602ce45a76a3"
     // call API
     fetch(url)
         .then(function(response) {
             // make sure the request was successful
             if (response.status != 200) {
                 return {
-                    text: "Error calling the Numbers API service: " + response.statusText
+                    text: "Error calling the News API service: " + response.statusText
                 }
             }
             return response.json();
         }).then(function(json) {
         // update DOM with response
-        updateResult(json.text);
+        updateResult(json);
     });
 }
 
-function updateResult(info) {
-    document.getElementById('results').textContent = info;
+function updateResult(json) {
+    let results = [];
+    results += '<div class="border mx-5 mb-5">';
+    results += '<div class="px-3">';
+    results += '<p class="pt-2" style="font-size: 13px">About {json.totalResults} results</p>\n';
+    results += '</div>';
+    results += '</div>';
+
+    document.getElementById("searchResults").innerHTML = results;
+
 }
 
 document.getElementById('woo').addEventListener('click', onClick);
